a01857c693f4ab5f18af21a86198314bb86fb509
kybernetyk <leon@celestialmachines.com>
2014-06-01 21:48:05 +0200

    lol

--- nowplaying.m
+++ nowplaying.m
@@ -3,77 +3,77 @@
 #import "Spotify.h"
 
 NSString *nowPlayingInItunes () {
-	iTunesApplication *iTunes = nil;
-	iTunesTrack *currentTrack = nil;
-	NSString *trackName = nil;
-	NSString *artistName = nil;
-	NSString *returnString = nil;
-	
-	iTunes = [SBApplication applicationWithBundleIdentifier:@"com.apple.iTunes"];
-	if (!iTunes) {
-		//NSLog(@"iTunes handle == nil!");
-		return nil;
-	}
+    iTunesApplication *iTunes = nil;
+    iTunesTrack *currentTrack = nil;
+    NSString *trackName = nil;
+    NSString *artistName = nil;
+    NSString *returnString = nil;
     
-	
-	/*
-	 now follows a bunch of try/catch stuff and seemingly senseless string copying + double checking.
-	 but we must do it this way as the iTunes application could change the track anytime which would
-	 lead to invalid/partial data.
-	 
-	 the joy of concurrency :)
-	 */
+    iTunes = [SBApplication applicationWithBundleIdentifier:@"com.apple.iTunes"];
+    if (!iTunes) {
+        //NSLog(@"iTunes handle == nil!");
+        return nil;
+    }
     
-	if (![iTunes isRunning]) {
-		//NSLog(@"iTunes not running == nil!");
-		return nil;
-	}
-	
-	@try {
-		currentTrack = [[iTunes currentTrack] get];
-	}
-	@catch(NSException *e) {
-		//NSLog(@"#2 Exception:%@ Reason: %@ Callstack: %@ userInfo: %@",e, [e reason], [e callStackSymbols],[e userInfo] );
-		return nil;
-	}
     
-	if ([currentTrack exists] && [iTunes isRunning]) {
-		@try {
-			trackName = [currentTrack name];
-			if (trackName != nil)
-				trackName= [NSString stringWithString: trackName];
-			
-			artistName = [currentTrack artist];
-			if (artistName != nil)
-				artistName = [NSString stringWithString: artistName];
-		}
-		@catch (NSException *e) {
-			/*NSLog(@"#3 Exception:%@ Reason: %@ Callstack: %@ userInfo: %@",e, [e reason], [e callStackSymbols],[e userInfo] );
+    /*
+     now follows a bunch of try/catch stuff and seemingly senseless string copying + double checking.
+     but we must do it this way as the iTunes application could change the track anytime which would
+     lead to invalid/partial data.
+     
+     the joy of concurrency :)
+     */
+    
+    if (![iTunes isRunning]) {
+        //NSLog(@"iTunes not running == nil!");
+        return nil;
+    }
+    
+    @try {
+        currentTrack = [[iTunes currentTrack] get];
+    }
+    @catch(NSException *e) {
+        //NSLog(@"#2 Exception:%@ Reason: %@ Callstack: %@ userInfo: %@",e, [e reason], [e callStackSymbols],[e userInfo] );
+        return nil;
+    }
+    
+    if ([currentTrack exists] && [iTunes isRunning]) {
+        @try {
+            trackName = [currentTrack name];
+            if (trackName != nil)
+                trackName= [NSString stringWithString: trackName];
+            
+            artistName = [currentTrack artist];
+            if (artistName != nil)
+                artistName = [NSString stringWithString: artistName];
+        }
+        @catch (NSException *e) {
+            /*NSLog(@"#3 Exception:%@ Reason: %@ Callstack: %@ userInfo: %@",e, [e reason], [e callStackSymbols],[e userInfo] );
              NSLog(@"name: %@", trackName);
              NSLog(@"artist: %@", artistName);
              NSLog(@"kind: %@", kind);
              NSLog(@"stream: %@", streamTitle);*/
-			
-			return nil;
-		}
-		
-	}
+            
+            return nil;
+        }
+        
+    }
     
-	//now let's build our display string
-	if (artistName && trackName) {
-		returnString = [NSString stringWithFormat: @"%@ - %@", artistName, trackName];
+    //now let's build our display string
+    if (artistName && trackName) {
+        returnString = [NSString stringWithFormat: @"%@ - %@", artistName, trackName];
     }
-	if (artistName && !trackName) {
-		returnString = [NSString stringWithFormat: @"%@", artistName];
+    if (artistName && !trackName) {
+        returnString = [NSString stringWithFormat: @"%@", artistName];
     }
-	if (!artistName && trackName) {
-		returnString = [NSString stringWithFormat: @"%@", trackName];
+    if (!artistName && trackName) {
+        returnString = [NSString stringWithFormat: @"%@", trackName];
     }
-	if (!artistName && !trackName) { //just to be safe. computers are magic!
-		return nil;
+    if (!artistName && !trackName) { //just to be safe. computers are magic!
+        return nil;
     }
-	
-	return returnString;
+    
+    return returnString;
 }
 
 
@@ -104,13 +104,13 @@ NSString *nowPlayingInSpotify() {
     NSString *trackArtist = [[track artist] copy];
     
     //now let's build our display string
-	if (trackArtist && trackName) {
+    if (trackArtist && trackName) {
         return [NSString stringWithFormat: @"%@ - %@", trackArtist, trackName];
     }
-	if (trackArtist && !trackName) {
-		return [NSString stringWithFormat: @"%@", trackArtist];
+    if (trackArtist && !trackName) {
+        return [NSString stringWithFormat: @"%@", trackArtist];
     }
-	if (!trackArtist && trackName) {
+    if (!trackArtist && trackName) {
         return [NSString stringWithFormat: @"%@", trackName];
     }
     
@@ -120,23 +120,23 @@ NSString *nowPlayingInSpotify() {
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     
-	
-	NSString *ns_nowplaying = nil;
+    
+    NSString *ns_nowplaying = nil;
     
     if (argc == 2 && strcmp(argv[1], "-s") == 0) {
         ns_nowplaying = nowPlayingInSpotify();
     } else {
         ns_nowplaying = nowPlayingInItunes();
     }
-	if (!ns_nowplaying)	{
-		fprintf(stderr, "Could not connect to server app ...\n");
-		return 1;
-	}
-	
-	const char *str_now_playing = [ns_nowplaying cStringUsingEncoding: NSUTF8StringEncoding];
-	
-	fprintf(stdout, "\u266C %s\n", str_now_playing);
+    if (!ns_nowplaying) {
+        fprintf(stderr, "Could not connect to server app ...\n");
+        return 1;
+    }
     
-	[pool drain];
+    const char *str_now_playing = [ns_nowplaying cStringUsingEncoding: NSUTF8StringEncoding];
+    
+    fprintf(stdout, "\u266C %s\n", str_now_playing);
+    
+    [pool drain];
     return 0;
 }
